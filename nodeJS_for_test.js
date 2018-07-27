9b6b4bb96e142cc0776c3bccfdb61772dbba5e39
Cherry <cherry.chien@sunspringautomation.com.tw>
2018-07-27 09:02:38 +0800

    Modify "Hello" from http to console.

--- nodeJS_for_test.js
+++ nodeJS_for_test.js
@@ -1,6 +1,1 @@
-var http = require('http');
-
-var httpServer = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World</h1>\n');
-});
+console.log("Hello!");
