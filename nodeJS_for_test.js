ce3a92185ca99b3bd465267a085c53aa4c07e199
Cherry <cherry.chien@sunspringautomation.com.tw>
2018-08-22 09:39:37 +0800

    Create server test

--- nodeJS_for_test.js
+++ nodeJS_for_test.js
@@ -1,1 +1,10 @@
-console.log("Hello!");
+var http = require('http');
+var server = http.createServer(function(request, response){
+console.log("Request received.");
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("Hello World");
+response.end();
+});
+
+server.listen(3000);
+console.log("Node.js web server at port 3000 is running.");
