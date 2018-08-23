49d917ee0790759025056950b538b4bfbb68765f
Cherry <cherry.chien@sunspringautomation.com.tw>
2018-08-23 08:48:25 +0800

    Modify Server that Restful api can send message to it.

--- nodeJS_for_test.js
+++ nodeJS_for_test.js
@@ -1,20 +1,22 @@
-var http = require('http');
-var Router = require('router');
-var server;
-var router = new Router();
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-server = http.createServer(function(req, resp) {
-    router(req, resp, function(msg){
-        if(!msg) {
-            resp.writeHead(404);
-        }
-        else {
-            console.log(msg.message, msg.stack);
-            resp.writeHead(400);
-        }
-        resp.end('RESTful API Server is running!');
-    });
-});
-server.listen(3000, function() {
-    console.log('Listening on port 3000');
-});
+app.get('/:id', function (req, res) {
+  // 首先我们读取已存在的用户
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      var user = data["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+  });
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:%s", port)
+
+})
